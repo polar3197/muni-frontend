5507bf86ca59a8b7b2b2fecb8fda500844772c8a
Charlie Cooper <charliepeople427@gmail.com>
2025-12-13 15:08:25 -0800

    update cloudflare url

--- muni.js
+++ muni.js
@@ -60,7 +60,7 @@ window.addEventListener('load', function() {
         vehicleMarkers = [];
         routeCounts = {};
         
-        fetch("https://another-northern-epinions-wallpaper.trycloudflare.com/vehicles/current", {
+        fetch("https://postings-gel-observer-undergraduate.trycloudflare.com/vehicles/current", {
             headers: { "ngrok-skip-browser-warning": "true" }
         })
         .then(response => response.json())
